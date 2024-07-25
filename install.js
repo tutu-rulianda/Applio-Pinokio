3b8ad743ed9d7014b0ab2edc758fadbe45ccdf1f
Tutu Rulianda <79007866+tutu-rulianda@users.noreply.github.com>
2024-07-25 16:42:59 +0700

    Update install.js

--- install.js
+++ install.js
@@ -44,9 +44,7 @@ module.exports = {
         path: "applio",
         message: [
           "pip install --upgrade setuptools",
-          "pip install certifi==2023.11.17",
           "pip install -r requirements.txt",
-          "pip install certifi==2023.11.17",
           "pip uninstall torch torchvision torchaudio -y",
           "pip install --pre torch torchvision torchaudio --index-url https://download.pytorch.org/whl/nightly/cpu",
         ],
