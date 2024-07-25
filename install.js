2343c846dec319f2aa9c1f8d9bc30f16a7786699
Tutu Rulianda <79007866+tutu-rulianda@users.noreply.github.com>
2024-07-25 16:27:41 +0700

    Update install.js
    Changed cuda to mps

--- install.js
+++ install.js
@@ -44,9 +44,9 @@ module.exports = {
         path: "applio",
         message: [
           "pip install --upgrade setuptools",
-          "pip install -r requirements.txt",
+          "#pip install -r requirements.txt",
           "pip uninstall torch torchvision torchaudio -y",
-          "pip install torch==2.1.1 torchvision==0.16.1 torchaudio==2.1.1 --index-url https://download.pytorch.org/whl/cu121",
+          "pip install --pre torch torchvision torchaudio --index-url https://download.pytorch.org/whl/nightly/cpu",
         ],
       }
     },
